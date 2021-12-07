efe7c28492efc25f81b49a3e49e07d1b9d99b0fc
RoyBuchanandev <roybuchanan1996@gmail.com>
2021-12-07 14:05:09 -0300

    se agrega proyecto

--- src/Components/ItemDetailsContainer/ItemDetail.js
+++ src/Components/ItemDetailsContainer/ItemDetail.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import { items } from '../../utils/Items'
 import Item from '../ItemListContainer/Item/Item'
+import ItemCount from '../ItemListContainer/Item/ItemCounter/ItemCount'
 
-export default function ItemDetail({item}) {
-    return  ( 
+
+
+
+export default function ItemDetail({ item, onAdd }) {
+  
+
+    return (
         <div>
-        <Item item= {item} />
+            <ItemCount onAdd ={onAdd}/>
+            <Item item={item} />
         </div>
-    )}
+
+    )
+}
+
+
+//1- agregar el item count dentro del itemDetail (hecho)
+//2-  pasarle por props al itemCount el onADD que ya estaba de desafios anteriores (hecho) 
+//3- cuando se le hace click a onAdd tiene que navegar a '/Cart'
+// que por el momento es un componente vacio
     
 
--- src/Components/ItemListContainer/Item/ItemCounter/ItemCount.jsx
+++ src/Components/ItemListContainer/Item/ItemCounter/ItemCount.jsx
@@ -1,26 +1,36 @@
 import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
-import "./ItemCount.scss" 
+import "./ItemCount.scss";
+import { Link } from 'react-router-dom'
 
 const ItemCount = ({ initial, stock, onAdd }) => {
   const [count, setCount] = useState(initial);
 
+
   const handleOp = (simbolo) => {
-     
     simbolo === '-' ? count > 1 && setCount(count - 1) : count < stock && setCount(count + 1);
   };
 
   return (
-    <div className = "item-count-container">
-        <div className = "op-buttons-container"> 
-      <Button variant="outlined" className = "op-button" onClick={() => handleOp('-')}> - </Button>
-      <span>{count}</span>
-      <Button variant="outlined" className = "op-button" onClick={() => handleOp('+')}> + </Button>
+    <div className="item-count-container">
+      <div className="op-buttons-container">
+        <Button variant="outlined" className="op-button" onClick={() => handleOp('-')}> - </Button>
+        <span>{count}</span>
+        <Button variant="outlined" className="op-button" onClick={() => handleOp('+')}> + </Button>
+        <br />
       </div>
+
+      <div>
+        <Link to='/Cart' className='btn btn-outline-danger mt-3 mb-2' onClick={onAdd}><strong>Agregar al carrito: {count} unidad/es</strong></Link><br />
+
+      </div>
+
       <br />
-    
+
     </div>
   );
+
 };
+
 
 export default ItemCount;
